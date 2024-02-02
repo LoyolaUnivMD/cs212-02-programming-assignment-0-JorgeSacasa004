57c474029db66d61d8f4d2495c74cadbf37bad1c
JorgeSacasa004 <156947365+JorgeSacasa004@users.noreply.github.com>
2024-02-02 13:13:53 -0500

    Update PA0.java

--- PA0.java
+++ PA0.java
@@ -1,14 +1,32 @@
-//pa0
-import java.util.Scanner;  // Import the Scanner class
+import java.util.Scanner;
+public class Main
+{
+	public static void main(String[] args) {
+	    Scanner scan = new Scanner(System.in);
+		System.out.println("How many people will infect the virus daily? ");
+		double R = scan.nextInt();
+		double totalCases = 1;
+		int count = 0;
+		System.out.print("day        Cases\n");  
+		while (count < 10){
+		    if (count == 0 ){
+		        System.out.print("0          1\n");
+		        count += 1;
+		    }
+		    else if(count == 1){
+		        double calculation = R * totalCases;
+		        totalCases = totalCases + calculation;
+		        System.out.print(count + "          " + calculation + "\n" ); 
+		        count += 1;
+		    }
+		    else{
+		        double calculation2 =  totalCases + (R*2);
+		        		        totalCases = totalCases + calculation2;
+		        System.out.print(count + "          " + calculation2 + "\n");
+		        count += 1;
+		    }
 
-public class PA0{
-    public static void main(String[] args){
-        Scanner scan = new Scanner(System.in);
-        System.out.print("What is the R number? ");
-        float R_number = scan.nextFloat();
-
-        System.out.println(R_number);
-
-
-    }
+		}
+		
+	}
 }
