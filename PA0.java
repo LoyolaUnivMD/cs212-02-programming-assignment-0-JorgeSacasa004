43f823a33fa92bfbad73a963eb536220c675dfef
JorgeSacasa004 <156947365+JorgeSacasa004@users.noreply.github.com>
2024-02-02 13:35:22 -0500

    Update PA0.java

--- PA0.java
+++ PA0.java
@@ -10,19 +10,13 @@ public class Main
 		System.out.print("day        Cases\n");  
 		while (count < 10){
 		    if (count == 0 ){
-		        System.out.print("0          1\n");
-		        count += 1;
-		    }
-		    else if(count == 1){
-		        double calculation = R * totalCases;
-		        totalCases = totalCases + calculation;
-		        System.out.print(count + "          " + calculation + "\n" ); 
+		        System.out.print("0          " + totalCases +"\n");
 		        count += 1;
 		    }
 		    else{
-		        double calculation2 =  totalCases + (R*2);
-		        		        totalCases = totalCases + calculation2;
-		        System.out.print(count + "          " + calculation2 + "\n");
+		        double calculation = (R * totalCases)+ 1;
+		        totalCases = calculation;
+		        System.out.print(count + "          " + calculation + "\n" ); 
 		        count += 1;
 		    }
 
